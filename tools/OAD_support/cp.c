6ff5ab26db5d5687858794162c51d71fd67e0ff7
Kaiyuan Hou <khou2020@outlook.com>
2018-10-02 01:58:20 -0500

    fix

--- tools/OAD_support/cp.c
+++ tools/OAD_support/cp.c
@@ -81,7 +81,7 @@ cp_fd* cp_wr_open(char* fname, size_t fsize){
 
     topen = getwalltime();
 
-    fd->fd = open(fname, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    fd->fd = open(fname, O_CREAT | O_WRONLY | O_TRUNC | O_DIRECT | O_SYNC, 0644);
 
     return fd;
 }
@@ -102,14 +102,6 @@ int cp_wr_close(cp_fd *fd){
 #endif
 
     t1 = getwalltime();
-    fsync(fd->fd);
-    t2 = getwalltime();
-    
-#ifdef ALLOW_USE_MPI
-    MPI_Barrier(MPI_COMM_WORLD);
-#endif
-
-    t3 = getwalltime();
 
     wsize = 0;
     while(fd->abuf < fd->cbuf){
@@ -119,9 +111,8 @@ int cp_wr_close(cp_fd *fd){
         }
         fd->abuf += ioret;
     }
-    fsync(fd->fd);
 
-    wr_time = getwalltime() - t3 - t2 + t1;
+    wr_time = getwalltime() - t1;
 
     close(fd->fd);
 
@@ -149,7 +140,7 @@ cp_fd* cp_rd_open(char* fname){
 
     topen = getwalltime();
 
-    fd->fd = open(fname, O_RDONLY, 0644);
+    fd->fd = open(fname, O_RDONLY | O_DIRECT | O_SYNC, 0644);
 
 #ifdef ALLOW_USE_MPI
     MPI_Barrier(MPI_COMM_WORLD);
